b8f75f481a3ab5310e77037890df604d008abe61
Shaady Dawood <shaady@sdawood.com>
2015-06-15 07:28:13 +1000

    mergeIntoBasic reduces left and generates a mock 1. branch inter-nodes are created in first pass 2. not yet capable of walking a mock-sours in the same pass
    Nest:
    Implement above
    implement right reduce, with recursion to create missing paths right to left

--- test/snapshot-mock.js
+++ test/snapshot-mock.js
@@ -22,17 +22,125 @@ var _ = require('lodash');
 var ContextManager = require('../lib/context').ContextManager;
 var _provides = require('../lib/reactaz/tagspm_modules/builtins/provides');
 var MemorySubject = _provides.MemorySubject,
-  RequirementProvider = _provides.RequirementProvider;
+  RequirementProvider = _provides.RequirementProvider,
+  unparse = _provides.unparse;
 
 
-suite('snapshot-mocking', function() {
-  test('MemorySubject: tag a selective snapshot of an available source of data, and reconstruct it in your own scope', function () {
+suite('builtins#snapshot#mocking', function() {
+
+  test('builtins#unparse: tag a selective snapshot of an available source of data, and reconstruct it in your own scope', function () {
+    var ctx = new ContextManager();
+    var jpql = require('../lib/index');
+    var source = { x: 'X', y: {yy: "YY",  junk: '$#%^&#!@#$'},
+      z: "Z", w: [
+        { w: { w: {name: "ReacTaz"}}},
+        "w1"
+      ], junk: '$#%^&#!@#$'};
+    var snapshotpath = '$[x, y.yy, z, w[.w.w.name]]'; //@todo #takeAll activeScript Tag to get leaf or node.*, in this example we had to know that x, y are keys with simple values
+    var flatResult = jpql.nodes(source, snapshotpath);
+    assert.deepEqual(flatResult, [
+      {
+        "path": [
+          "$",
+          "x"
+        ],
+        "value": "X"
+      },
+      {
+        "path": [
+          "$",
+          "y",
+          "yy"
+        ],
+        "value": "YY"
+      },
+      {
+        "path": [
+          "$",
+          "z"
+        ],
+        "value": "Z"
+      },
+      {
+        "path": [
+          "$",
+          "w",
+          0
+        ],
+        "value": "W0"
+      },
+      {
+        "path": [
+          "$",
+          "w",
+          1
+        ],
+        "value": "w1"
+      }
+    ]);
+    var pocMock = {};
+    assert.deepEqual(unparse(pocMock, flatResult, '#basic'), {
+      "0": "W0",
+      "1": "w1",
+      "x": "X",
+      "yy": "YY",
+      "z": "Z"
+    });
+    pocMock = {};
+    assert.deepEqual(unparse(pocMock, flatResult, '#keysNaN'), {not: 'equal'});
+    var mockClone = _.merge({}, pocMock);
+    var foreignNodes = [{path: 'unknown.com', value: 'untrusted $#%^&#!@#$ junk'}];
+    var through = unparse(mockClone, foreignNodes);
+    // did we let alien nodes into the mock?
+    assert.deepEqual(mockClone, pocMock);
+  });
+
+  test('builtins#MemorySubject: tag a selective snapshot of an available source of data, and reconstruct it in your own scope', function () {
     var ctx = new ContextManager();
     var jpql = require('../lib/index');
     var source = {x: 'X', y: {yy: "YY",  junk: '$#%^&#!@#$'}, z: "Z", w: ["W0", "w1"], junk: '$#%^&#!@#$'};
-    var snapshotpath = '$[x.*, y.yy, z.*, w.*]';
+    var snapshotpath = '$[x, y.yy, z, w.*]'; //@todo #takeAll activeScript Tag to get leaf or node.*, in this example we had to know that x, y are keys with simple values
     var provider = new MemorySubject(jpql.nodes(source, snapshotpath), snapshotpath, ctx);
-    assert.deepEqual(provider.nodes, [false]);
+    assert.deepEqual(provider.nodes, [
+      {
+        "path": [
+          "$",
+          "x"
+        ],
+        "value": "X"
+      },
+      {
+        "path": [
+          "$",
+          "y",
+          "yy"
+        ],
+        "value": "YY"
+      },
+      {
+        "path": [
+          "$",
+          "z"
+        ],
+        "value": "Z"
+      },
+      {
+        "path": [
+          "$",
+          "w",
+          0
+        ],
+        "value": "W0"
+      },
+      {
+        "path": [
+          "$",
+          "w",
+          1
+        ],
+        "value": "w1"
+      }
+    ]); //flatResult
     var pocMock = {};
     assert.deepEqual(provider.provideAs(pocMock), {not: 'equal'});
     var mockClone = _.merge({}, pocMock);
@@ -42,12 +150,12 @@ suite('snapshot-mocking', function() {
     assert.deepEqual(mockClone, pocMock);
   });
 
-  test('tag a selective snapshot of an available source of data, and reconstruct it in your own scope', function () {
+  test('builtins#RequirementProvider: tag a selective snapshot of an available source of data, and reconstruct it in your own scope', function () {
     var ctx = new ContextManager();
     var jpql = require('../lib/index');
     var source = {x: 'X', y: {yy: "YY",  junk: '$#%^&#!@#$'}, z: "Z", w: ["W0", "w1"], junk: '$#%^&#!@#$'};
     var snapshotpath = '$[x.*, y.yy, z.*, w.*]';
-    var provider = new ReqProvider(jpql.nodes(source, snapshotpath), snapshotpath, ctx);
+    var provider = new RequirementProvider(jpql.nodes(source, snapshotpath), snapshotpath, ctx);
     assert.deepEqual(provider.nodes, [false]);
     var pocMock = {};
     assert.deepEqual(provider.provideAs(pocMock), {not: 'equal'});
